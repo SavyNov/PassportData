80dc566df2f9799afb87e2e79811f0e3e183dde5
SavyNov <57320257+SavyNov@users.noreply.github.com>
2023-08-11 11:53:28 +0300

    Add files via upload

---
+++ PassportData/PassportData/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PassportData.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PASSPORTDATA_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PASSPORTDATA                107
+#define IDI_SMALL                       108
+#define IDC_PASSPORTDATA                109
+#define IDR_MAINFRAME                   128
+#define IDD_PASSPORT                    129
+#define IDC_SURNAME                     1000
+#define IDC_GIVENNAME                   1001
+#define IDC_NATIONALITY                 1002
+#define IDC_BDATE                       1003
+#define IDC_Gender                      1004
+#define IDC_IDate                       1005
+#define IDC_EDATE                       1006
+#define IDC_EGN                         1007
+#define IDC_PBIRTH                      1008
+#define IDC_PBIRTH2                     1009
+#define IDC_NRAND                       1009
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
